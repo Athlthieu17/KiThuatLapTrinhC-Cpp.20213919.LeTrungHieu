f27de0e1fd400e99a2a240a865484cb4dc8a532f
HieuAth <hieu.lthust170703@gmail.com>
2022-11-17 13:10:35 +0700

    17T11

---
+++ Learning/Passing_Arguments_ByAddress/Passing_Arguments_ByAddress/Passing_Arguments_ByAddress_Cpp.cpp
@@ -0,0 +1,103 @@
+﻿#include <iostream>
+using namespace std;
+
+void foo(int* ptr)
+{
+	*ptr = 20;
+}
+
+void printArray(const int* array, int length)
+{
+	// kiểm tra con trỏ null
+	if (!array)
+		return;
+
+	for (int index = 0; index < length; ++index)
+		cout << array[index] << ' ';
+}
+
+// tempPtr là bản sao của ptr
+void setToNull(int* tempPtr)
+{
+	// trỏ tempPtr đến một vị trí khác, không phải thay đổi giá trị tempPtr trỏ tới
+	tempPtr = nullptr;
+}
+
+void setValue(int* tempPtr)
+{
+	*tempPtr = 20; // thay đổi giá trị tempPtr và ptr trỏ tới
+}
+
+void setToNullUp(int*& tempPtr)
+{
+	tempPtr = nullptr;
+}
+
+int main()
+{
+	int value = 10;
+
+	cout << "value = " << value << '\n';
+	foo(&value);
+	cout << "value = " << value << '\n';
+
+	//Output value = 10 
+	// value = 20
+
+	int array[6] = { 6, 5, 4, 3, 2, 1 };
+	printArray(array, 6);
+
+	//Output: 6 5 4 3 2 1
+
+	// Truyền địa chỉ cho hàm bằng giá trị
+
+	int value_1 = 10;
+	int* ptr = &value_1;
+
+	cout << *ptr << "\n"; // 10
+
+	// tempPtr là bản sao của ptr
+	setToNull(ptr);
+
+	if (ptr)
+		cout << *ptr << "\n"; // 10
+	else
+		cout << " ptr is null";
+
+	/*
+	Khi truyền một đối số theo địa chỉ, tham số hàm nhận được một bản sao của địa chỉ từ đối số. 
+	Tại thời điểm này, tham số hàm và đối số đều trỏ đến cùng một giá trị.
+	Nếu thay đổi giá trị được tham số hàm trỏ đến, điều đó sẽ thay đổi giá trị mà đối số đang trỏ đến, 
+	vì cả tham số hàm và đối số đều trỏ đến cùng một giá trị.
+	Nếu tham số hàm được gán một địa chỉ khác, điều đó sẽ không ảnh hưởng đến đối số, vì tham số hàm chỉ là bản sao của đối số.
+	*/
+	int value_2 = 10;
+	int* ptr_1 = &value_2;
+
+	cout << *ptr_1 << "\n"; // 10
+
+	// tempPtr là bản sao của ptr
+	setValue(ptr_1);
+
+	if (ptr_1)
+		cout << *ptr_1 << "\n"; // 20
+	else
+		cout << " ptr_1 is null";
+
+	//Truyền địa chỉ cho hàm bằng tham chiếu
+	int value_3 = 10;
+	int* ptr_2 = &value_3;
+
+	cout << *ptr_2 << "\n"; // 10
+
+	// tempPtr là tham chiếu đến ptr
+	setToNullUp(ptr_2);
+
+	if (ptr_2)
+		cout << *ptr_2 << "\n";
+	else
+		cout << "ptr_2 is null" << "\n"; //ptr_2 is null
+
+	system("pause");
+	return 0;
+}
---
+++ Learning/Return_Function_Type/Return_Function_Type/Return_Function_Type_Cpp.cpp
@@ -0,0 +1,72 @@
+﻿#include <iostream>
+#include <array>
+using namespace std;
+#define MAX 10
+//Hàm trả về giá trị (return by value)
+int add(int x, int y)
+{
+	int value = x + y;
+	return value; // Bản sao của biến value được tạo ra và trả về
+} // biến value bị hủy khi ra khỏi hàm
+
+int return69()
+{
+	return (60 + 9); // Bản sao của hằng 69 được tạo ra và trả về
+}
+
+//Hàm trả về địa chỉ(return by address)
+int* add_2(int x, int y)
+{
+	int value = x + y;
+	return &value; // trả về địa chỉ của biến value
+} // biến value bị hủy khi ra khỏi hàm
+
+
+int* allocateArray(int size)
+{
+	return new int[size];
+}
+
+//Hàm trả về tham chiếu (return by reference)
+int& add_3(int x, int y)
+{
+	int value = x + y;
+	return value; // trả về tham chiếu của biến value
+} // biến value bị hủy khi ra khỏi hàm
+
+
+int& getElement(array<int, MAX>& arr, int idx)
+{
+	return arr[idx]; // trả về tham chiếu tới phần tử idx
+}
+
+int& max(int& a, int& b)
+{
+	return a > b ? a : b; // trả về tham chiếu của biến lớn hơn
+}
+
+int main()
+{
+	int* array_2 = allocateArray(10);
+
+	// ...
+
+	delete[] array_2;
+	array<int, MAX> arr;
+
+	// gán phần tử thứ 2 = 20 bằng tham chiếu <=> arr[2] = 20
+	getElement(arr, 2) = 20;
+
+	cout << arr[2] << '\n'; // in 20
+
+
+	int x = 6;
+	int y = 9;
+
+	max(x, y) = 69; // gán biến lớn hơn bằng 69
+
+	cout << "x = " << x << '\n'; // 6
+	cout << "y = " << y << '\n'; // 69: y lớn hơn nên bị thay đổi
+	system("pause");
+	return 0;
+}
