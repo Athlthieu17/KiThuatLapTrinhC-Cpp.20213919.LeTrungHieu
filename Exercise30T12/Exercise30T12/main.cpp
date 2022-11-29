ba14ec1426b6e4a1b8b42f1567150a6d83801677
HieuAth <hieu.lthust170703@gmail.com>
2022-11-29 13:05:07 +0700

    finishEx30T12

--- Exercise30T12/Exercise30T12/main.cpp
+++ Exercise30T12/Exercise30T12/main.cpp
@@ -1,4 +1,5 @@
-#include<iostream>
+﻿#include<iostream>
+#include <cstring>
 using namespace std;
 
 class Complex
@@ -49,55 +50,138 @@ class Person
 public:
     void cinData()
     {
-        cout << "Moi ban nhap ten"; cin.getline(Name, 50);
-        cout << "Moi ban nhap ngay thang nam sinh"; cin >> Day >> Month >> Year;
-        cout << "Moi ban nhap que quan"; cin.getline(HomeTown, 100);
+        cout << "Moi ban nhap ten:"; cin.getline(Name, 50);
+        cout << "Moi ban nhap ngay thang nam sinh:"; cin >> Day >> Month >> Year;
+        cin.ignore(1);
+        cout << "Moi ban nhap que quan:"; cin.getline(HomeTown, 100);
     }
     void exportData()
     {
-        cout << "ten" << Name;
-        cout << Day + "\\" + Month << +"\\" + Year;
-        cout<< "que quan " << HomeTown;
+        cout << "Ten cua ban la:" << Name;
+        cout << "\nNgay thang nam sinh:";
+        cout << Day<<"/"<<Month <<"/"<<Year;
+        cout<< "\nQue quan cua ban: " << HomeTown;
     }
-    void setName(char _Name[50])
+    void setName(char* _Name[50])
     {
-        Name = _Name;
+        char* strcpy(char* Name,char* _Name);
     }
-    char getName()
+    char* getName()
     {
         return Name;
     }
 
-    void setHomeTown(char HomeTown[50])
+    void setHomeTown(char* _HomeTown[50])
     {
-        this->HomeTown = HomeTown;
+        char* strcpy(char* HomeTown, char* _HomeTown);
     }
-    char getHomeTown()
+    char* getHomeTown()
+    {
+        return HomeTown;
+    }
+
+    void setDate(int _Day, int _Month, int _Year)
+    {
+        Day = _Day;
+        Month = _Month;
+        Year = _Year;
+    }
+
+    void getDate(int& _Day, int& _Month, int& _Year)
+    {
+        _Day = Day;
+        _Month = Month;
+        _Year = Year;
+    }
+
+};
+
+
+class Date
+{
+protected:
+    int day, month, year;
+public:
+    
+    void cinDate()
+    {
+        cout << "Moi ban nhap ngay thang nam sinh:"; cin >> day >> month >> year;
+    }
+    void exportDate()
+    {
+        cout << "Ngay sinh cua ban:" << day << '/' << month << '/' << year;
+    }
+    void setDate(int _day, int _month, int _year)
+    {
+        day = _day;
+        month = _month;
+        year = _year;
+    }
+    void getDate(int& _day, int& _month, int& _year)
+    {
+        _day = day;
+        _month = month;
+        _year = year;
+    }
+}; 
+class Person_2:Date
+{
+    char Name[50];
+    Date date;
+    char HomeTown[100];
+public:
+    void importPerson()
+    {
+        cout << "Moi ban nhap ten:"; cin.getline(Name, 50);
+        date.cinDate();
+        cin.ignore(1);
+        cout << "Moi ban nhap que quan:"; cin.getline(HomeTown, 100);
+    }
+    void exportPerson()
+    {
+        cout << "Ten cua ban la:" << Name;
+        cout << "\n";
+        date.exportDate();
+        cout << "\nQue quan cua ban: " << HomeTown;
+    }
+    void setName(char* _Name[50])
+    {
+        char* strcpy(char* Name,char* _Name);
+    }
+    char* getName()
+    {
+        return Name;
+    }
+
+    void setHomeTown(char* _HomeTown[50])
+    {
+        char* strcpy(char* HomeTown, char* _HomeTown);
+    }
+    char* getHomeTown()
     {
         return HomeTown;
     }
 
     void setDate(int Day, int Month, int Year)
     {
-        this->Day = Day;
-        this->Month = Month;
-        this->Year = Year;
+       day = Day;
+       month = Month;
+       year = Year;
     }
 
     void getDate(int& Day, int& Month, int& Year)
     {
-        Day = this->Day;
-        Month = this->Month;
-        Year = this->Year;
+        Day = day;
+        Month = month;
+        Year = year;
     }
-
-};
-
-class Person_2
-{
-
 };
 int main()
 {
-   
+    Person u1;
+    Date ntns;
+    u1.setDate(17, 07, 2003);
+    u1.exportData();
+
+
 }
